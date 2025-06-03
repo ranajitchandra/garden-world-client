cf7705357ca511206e06f501d444795d3dd6c34a
ranajitchandra <ranajitchandradas3@gmail.com>
2025-06-04 01:13:10 +0600

    aos

--- src/layouts/RootMyTips.jsx
+++ src/layouts/RootMyTips.jsx
@@ -3,9 +3,21 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 
 export default function RootMyTips() {
+
+    useEffect(() => {
+        AOS.init({
+            duration: 1000, // Animation duration in milliseconds
+            offset: 100, // Offset from the trigger point
+            easing: "ease-in-out", // Animation easing
+            delay: 100, // Delay in milliseconds
+            once: true, // Whether animation should happen only once
+        });
+    }, []);
 
     return (
         <>
--- src/layouts/RootTips.jsx
+++ src/layouts/RootTips.jsx
@@ -1,6 +1,8 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 
 export default function RootTips() {
